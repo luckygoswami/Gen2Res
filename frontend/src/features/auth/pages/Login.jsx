67e4a53bc38ba875cb087c568970edd098587951
Lucky Goswami <125822925+luckygoswami@users.noreply.github.com>
2026-05-04 19:23:59 +0000

    feat: implement Login and Register components with form handling and navigation

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,5 +1,52 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router';
+import { useAuth } from '@/features/auth/hooks/useAuth';
 
 export function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { loading, handleLogin } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await handleLogin({ email, password });
+      navigate('/');
+    } catch (err) {}
+  };
+
+  if (loading) return <main>Loading...</main>;
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <p>
+        Don't have an account? <Link to={'/register'}>Register</Link>
+      </p>
+    </main>
+  );
 }
--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,5 +1,62 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router';
+import { useAuth } from '@/features/auth/hooks/useAuth';
 
 export function Register() {
-  return <div>Register</div>;
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { loading, handleRegister } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await handleRegister({ username, email, password });
+      navigate('/');
+    } catch (err) {}
+  };
+
+  if (loading) return <main>Loading...</main>;
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+      <p>
+        Already have an account? <Link to={'/login'}>Login</Link>
+      </p>
+    </main>
+  );
 }
